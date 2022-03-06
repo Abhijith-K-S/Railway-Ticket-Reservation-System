524ef182d0b6b2244cc9c93e358c4e731e49aead
Abhijith K S <abhijithks1729@gmail.com>
2022-03-06 21:58:02 +0530

    Logic complete

--- reservation.asm
+++ reservation.asm
@@ -27,15 +27,30 @@ data segment
     classSelection db 0ah,0dh,'Select a class: $'
     classAName db 0ah,0dh,'1: CLASS A $' 
     classBName db 0ah,0dh,'2: CLASS B $'
-    classCName db 0ah,0dh,'3: CLASS C $'  
+    classCName db 0ah,0dh,'3: CLASS C $'
     
     ;error message
     errorString db 0ah,0dh,'INVALID OPTION.PLEASE TRY AGAIN!!!$'
+
+    ;status messages
+    currentStatus db 0ah,0dh,'Current Status: $'  
+    noMoreAvailable db 0ah,0dh,'Sorry!! No more seats are available $'
+    returnClassMenu db 0ah,0dh,'1: Return to class selection $'
+    returnMainMenu db 0ah,0dh,'2: Return to main menu $'
+    exitProg db 0ah,0dh,'3: Exit $'
+    choiceRead db 0ah,0dh,'Enter choice: $'
+    availableSeats db 0ah,0dh,'No. of seats available: $'
+    numOfSeatsToBeBooked db 0ah,0dh,'Enter no .of seats to be booked,[max 5]: $'    
+    limitOver db 0ah,0dh,'Maximum limit exceeded,give less than 5: $'
+    success db 0ah,0dh,'Booking successful! $'
+    bookedSeats db 0ah,0dh,'Your seats are: $'
+    anotherTicket db 0ah,0dh,'1: Book another ticket $'
+    exitProgram db 0ah,0dh,'2: Exit $' 
    
     ;variables to store available seating information
-    trainASeatsNumber db 3 dup(14)
-    trainBSeatsNumber db 3 dup(14)   
-    trainCSeatsNumber db 3 dup(14) 
+    trainASeatsNumber db 3 dup(20)
+    trainBSeatsNumber db 3 dup(20)   
+    trainCSeatsNumber db 3 dup(20) 
 
     ;variables to display booked/unbooked seats
     trainASeats db 60 dup(0)  
@@ -46,12 +61,12 @@ data segment
     currentlyChosenTrain db ?
     currentlyChosenClass db ? 
     
-    temp dw ?           
-    availableSeats db 0ah,0dh,'No. of seats available: $'
-    numOfSeatsToBeBooked db 0ah,0dh,'Enter no .of seats to be booked,[max 5]: $'    
-    limitOver db 0ah,0dh,'Maximum limit exceeded,give less than 5: $'
-    tempCount dw ?    
-    printVal db ? 
+    choiceCompartment dw ?     
+    choiceAvailableSeats dw ?
+    chosenTrainID db ?
+    chosenClassID db ?  
+    printVal db ?      
+
 data ends
     
 ;macro to print strings    
@@ -61,6 +76,13 @@ printString macro arg
     int 21h
 endm  
 
+;macro to print char
+printChar macro arg
+    mov dl,arg
+    mov ah,02h
+    int 21h
+endm
+
 ;code segment
 code segment
 assume cs:code,ds:data
@@ -68,7 +90,11 @@ assume cs:code,ds:data
 start:          mov ax,data
                 mov ds,ax
 ;print menu
-menu:           printString welcomeString  
+menu:           mov ah,00h
+                mov al,02h
+                int 10h
+                ;clear screen
+                printString welcomeString  
                 printString newlineString
 
 ;train menu
@@ -82,7 +108,6 @@ menu:           printString welcomeString
 chooseTrain:    printString newlineString 
                 printString trainSelection             
 
-
                 call readInt                
 ;check for errors
                 cmp al,04h
@@ -92,10 +117,10 @@ chooseTrain:    printString newlineString
                 jmp chooseTrain
 
 ;choose class
- noErrorTrain:  mov currentlyChosenTrain,al
+noErrorTrain:   mov currentlyChosenTrain,al
                 
                 printString newlineString
-                printString classMenuString  
+classMenu:      printString classMenuString  
                 printString newlineString  
                 printString classAName
                 printString newlineString
@@ -112,30 +137,101 @@ chooseClass:    printString newlineString
                 printString newlineString
                 printString errorString
                 jmp chooseClass
-  
-           
+             
 noErrorClass:   mov currentlyChosenClass,al
-
                 printString newlineString
+                printString currentStatus
+                printString newlineString
+                printString newlineString
+
+;set appropriate pointers
                 call displaySeats 
-                mov si,temp
-                mov di,tempCount
-                printString availableSeats
-                mov ah,[di]     
-                mov printVal,ah
+                mov si,choiceCompartment
+                mov di,choiceAvailableSeats
+                mov bh,[di]
+                mov ah,14h
+                sub ah,bh
+addLoop:        cmp ah,00h
+                jz h
+                inc si
+                dec ah
+                jmp addLoop
+
+;check if seats are available  
+h:              cmp bh,00h
+                jnz seatAvailable
+                printString noMoreAvailable
+                printString newlineString
+                printString returnClassMenu
+                printString returnMainMenu
+                printString exitProg
+redoChoice:     printString newlineString
+                printString choiceRead
+                
+                call readInt
+                cmp al,04h
+                jc process
+                printString errorString
+                jmp redoChoice
+process:        cmp al,01h
+                jz classMenu
+                cmp al,02h
+                jz menu
+                jmp exit
+
+seatAvailable:  printString availableSeats
+                mov printVal,bh
                 call printInt
+
 repeatBook:     printString newlineString 
                 printString numOfSeatsToBeBooked 
+                
                 call readInt  
                 cmp al,05h
-                jc exit
+                jc proceedBooking
                 printString limitOver
                 jmp repeatBook
-                
+
+proceedBooking: mov bl,al
+                mov bh,14h
+                mov ah,[di]
+                sub bh,ah
+                inc bh
+                printString newlineString
+                printString success
+                printString bookedSeats
+
+printSeats:     printChar chosenTrainID
+                printChar chosenClassID
+                mov printVal,bh
+                call printInt
+                inc bh
+                printChar ' '
+                mov [si],01h
+                inc si
+                dec [di]
+                dec bl
+                jnz printSeats
+
+;what to do after booking
+whatNext:       printString newlineString
+                printString anotherTicket
+                printString exitProgram
+                printString choiceRead
+                call readInt
+;check for errors 
+                cmp al,04h
+                jc noProbs
+                printString newlineString
+                printString errorString
+                jmp whatNext
+
+noProbs:        cmp al,01h
+                jz menu 
 
 exit:           mov ah,4ch
                 int 21h 
-                
+;end of main program               
 
 ;procedure to read integer
 readInt proc   
@@ -151,26 +247,26 @@ endp
 
 ;procedure to print integer
 printInt proc  
-    mov dl,printVal
-    and dl,0F0h
-    mov cl,04h
-    shr dl,cl
-    add dl,30h
-    cmp dl,39h
-    jc rnu
-    jz rnu  
-    add dl,07h
- rnu:mov ah,02h
-    int 21h
-    mov dl,printVal
-    and dl,0Fh
-    add dl,30h
-    cmp dl,39h
-    jc rnp
-    jz rnp  
-    add dl,07h
- rnp:mov ah,02h
-     int 21h
+        mov dl,printVal
+        and dl,0F0h
+        mov cl,04h
+        shr dl,cl
+        add dl,30h
+        cmp dl,39h
+        jc rnu
+        jz rnu  
+        add dl,07h
+ rnu:   mov ah,02h
+        int 21h
+        mov dl,printVal
+        and dl,0Fh
+        add dl,30h
+        cmp dl,39h
+        jc rnp
+        jz rnp  
+        add dl,07h
+ rnp:   mov ah,02h
+        int 21h
  ret
 endp   
     
@@ -182,33 +278,39 @@ trainAChosen:   cmp currentlyChosenTrain,01h
                 jnz trainBChosen
                 lea si,trainASeats   
                 lea di,trainASeatsNumber
+                mov chosenTrainID,'A'
                 jmp classAChosen
 
 trainBChosen:   cmp currentlyChosenTrain,02h
                 jnz trainCChosen
                 lea si,trainBSeats
                 lea di,trainBSeatsNumber
+                mov chosenTrainID,'B'
                 jmp classAChosen
 
 trainCChosen:   lea si,trainCSeats
                 lea di,trainCSeatsNumber
+                mov chosenTrainID,'C'
                 
 classAChosen:   cmp currentlyChosenClass,01h
                 jnz classBChosen
+                mov chosenClassID,'A'
                 jmp chooseOver
 
 classBChosen:   cmp currentlyChosenClass,02h
                 jnz classCChosen
                 add si,14h
                 add di,01h
+                mov chosenClassID,'B'
                 jmp chooseOver
 
 classCChosen:   add si,28h  
                 add di,02h
+                mov chosenClassID,'C'
 
-chooseOver:     mov tempCount,di
+chooseOver:     mov choiceAvailableSeats,di
                 mov di,si 
-                mov temp,si
+                mov choiceCompartment,si
                 inc di
                 mov ch,02h
 
@@ -244,6 +346,3 @@ endp
 
 code ends
 end start
-     
-     
-     
